0c9ba5e0329f487171bd505b0a8279dbcb3e7391
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-03-20 11:06:16 +0300

    Правила Checkstyle [#504981 #402314]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -3,23 +3,22 @@ package ru.job4j.checkstyle;
 public class Broken {
     private int sizeOfEmpty = 10;
 
-    public String surname;
+    private String surname;
 
-    public static final String NEWVALUE = "";
+    private String name;
 
-    void echo() { }
+    public static final String NEW_VALUE = "";
 
-    void media(Object obj) {
+    public void broken() { }
+
+    public void echo() { }
+
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
-
-    String name;
-
-    Broken() { }
-
 }
