9152d2d6dbd6ccdfba0aeff963749be693a4c126
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-04-09 17:22:21 +0300

    12.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/ExitAction.java
+++ src/main/java/ru/job4j/tracker/ExitAction.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 public class ExitAction implements UserAction {
+    private final Output out;
+
+    public ExitAction(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
         return "Exit";
@@ -8,7 +14,7 @@ public class ExitAction implements UserAction {
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        System.out.println("Good bye!");
+        out.println("=== Exit Program ===");
         return false;
     }
 }
