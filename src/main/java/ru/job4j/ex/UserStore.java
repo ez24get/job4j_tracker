486dbb6c2512624cc7fa18c078cdff93ff568e31
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-04-12 10:35:40 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #408866]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -6,11 +6,9 @@ public class UserStore {
         for (User user : users) {
             if (login.equals(user.getUsername())) {
                 return user;
-            } else {
-                throw new UserNotFoundException("User not found");
             }
         }
-        return null;
+        throw new UserNotFoundException("User not found");
     }
 
     public static boolean validate(User user) throws UserInvalidException {
