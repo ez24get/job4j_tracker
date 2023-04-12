326225eab05ffcffc064ed2922b9a243604014e8
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-04-12 10:18:12 +0300

    0.4. Пользовательские исключения. [#219367 #408812]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -6,10 +6,11 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
-            if (rsl == -1) {
-                throw new ElementNotFoundException("Element not found");
-            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found");
         }
         return rsl;
     }
