56d55cd3bb67258b6716ed1c5ebce6d2741ac25d
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-05-12 17:51:33 +0300

    0. Map, HashMap [#212652 #415390]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> usage = new HashMap<>();
         usage.put("johndowe@gmail.com", "John Ramires Dowe");
+        usage.put("johndowe@gmail.com", "Dowe");
+        usage.put("johndowe@gmail.com", "John R");
+        usage.put("john@gmail.com", "John Ramires Dowe");
+        usage.put("jo@gmail.com", "John Ramires Dowe");
         for (String key : usage.keySet()) {
             String value = usage.get(key);
             System.out.println(key + " = " + value);
