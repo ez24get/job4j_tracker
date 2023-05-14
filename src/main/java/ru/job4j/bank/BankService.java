7b36415fcbb5bf64222671ed7ddb6584e7f65dd2
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-05-14 15:00:17 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,72 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        if (!users.containsKey(user)) {
+            users.put(user, new ArrayList<>());
+        }
+    }
+
+    public boolean deleteUser(String passport) {
+        boolean rsl = false;
+        User user = findByPassport(passport);
+        if (users.containsKey(user)) {
+            users.remove(user);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (user != null && !users.get(user).contains(account)) {
+            users.get(user).add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        if (user != null) {
+            for (Account account : users.get(user)) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account srcUser = findByRequisite(srcPassport, srcRequisite);
+        Account destUser = findByRequisite(destPassport, destRequisite);
+        if (srcUser != null && destUser != null
+                && srcUser.getBalance() >= amount) {
+            srcUser.setBalance(srcUser.getBalance() - amount);
+            destUser.setBalance(destUser.getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
