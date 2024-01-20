8e4f19245e9bb08a2a3cadbe95d2e4d64f252bb1
michaelberezovski <berezovskii.mikhail@gmail.com>
2024-01-20 09:24:20 +0300

    2. Трекер SQL [#1734 #473973]

---
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -0,0 +1,149 @@
+package ru.job4j.tracker;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class SqlTracker implements Store {
+
+    private Connection connection;
+
+    public SqlTracker() {
+        init();
+    }
+
+    public SqlTracker(Connection connection) {
+        this.connection = connection;
+    }
+
+    private void init() {
+        try (InputStream input = SqlTracker.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            Properties config = new Properties();
+            config.load(input);
+            Class.forName(config.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void close() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    @Override
+    public Item add(Item item) {
+        Timestamp timestampFromLDT = Timestamp.valueOf(item.getCreated());
+        try (PreparedStatement statement =
+                     connection.prepareStatement("INSERT INTO items(name, created) VALUES (?, ?)",
+                             Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, item.getName());
+            statement.setTimestamp(2, timestampFromLDT);
+            statement.execute();
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    item.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+
+    @Override
+    public boolean replace(int id, Item item) {
+        Timestamp timestampFromLDT = Timestamp.valueOf(item.getCreated());
+        int count = 0;
+        boolean result = false;
+        try (PreparedStatement statement =
+                     connection.prepareStatement("UPDATE items SET name = ?, created = ? where id = ?;")) {
+            statement.setString(1, item.getName());
+            statement.setTimestamp(2, timestampFromLDT);
+            statement.setInt(3, id);
+            statement.execute();
+            count = statement.getUpdateCount();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (count > 0) {
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public void delete(int id) {
+        try (PreparedStatement statement =
+                     connection.prepareStatement("DELETE FROM items where id = ?;")) {
+            statement.setInt(1, id);
+            statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Item> findAll() {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM cities")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    items.add(new Item(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name")
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM cities")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    if (resultSet.getString("name").equals(key)) {
+                        items.add(new Item(
+                                resultSet.getInt("id"),
+                                resultSet.getString("name")
+                        ));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    @Override
+    public Item findById(int id) {
+        Item item = new Item();
+        try (PreparedStatement statement =
+                     connection.prepareStatement("SELECT id, name FROM cities WHERE id = ?")) {
+            statement.setInt(1, id);
+            statement.execute();
+            try (ResultSet resultSet = statement.executeQuery()) {
+                item = new Item(resultSet.getInt("id"), resultSet.getString("name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return item;
+    }
+}
